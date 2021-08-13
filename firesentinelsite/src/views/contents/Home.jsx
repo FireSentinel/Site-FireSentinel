0f57b972bf6532c912c6cc3312ef2f5bc0347691
FireSentinel <sentinel.fire.sent@gmail.com>
2021-08-13 18:49:42 +0000

    Funcionando 2.0
    Signed-off-by: FireSentinel <sentinel.fire.sent@gmail.com>

--- firesentinelsite/src/views/contents/Home.jsx
+++ firesentinelsite/src/views/contents/Home.jsx
@@ -3,9 +3,9 @@ import icon_firesentinel from "/workspace/Site-FireSentinel/firesentinelsite/src
 
 const Home = props => (
     <div>
-        <h1>Início</h1>
+        <br/><br/>
         <img src={icon_firesentinel} alt="Erro ao carregar imagem"/>
-        <h1>Fire Sentinel</h1>
+        <h1>FireSentinel</h1>
         <h2>Um sistema de vigilancia 24 horas </h2>
     </div>
 )
